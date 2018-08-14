2ded60cdf8d1166ba48982ce289698944d5ed35f
Hugh Mackworth <mackworth@aol.com>
2018-08-13 18:46:30 -0700

    More dark Mode tweaks

--- cTiVo/MTProgressindicator.m
+++ cTiVo/MTProgressindicator.m
@@ -11,12 +11,10 @@
 
 @implementation MTProgressindicator
 
-- (id)initWithFrame:(NSRect)frame
-{
+- (id)initWithFrame:(NSRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code here.
-        _displayProgress = NO;
         self.leftText = [[NSTextField alloc ] initWithFrame:frame];
         self.rightText =  [[NSTextField alloc ] initWithFrame:CGRectZero] ;
         [self setItemPropertiesToDefault:self ];
@@ -24,29 +22,39 @@
     
     return self;
 }
+
 -(void) awakeFromNib {
 	[super awakeFromNib];
     [self setItemPropertiesToDefault:self];
 }
 
+-(void) setFrame: (NSRect) frame {
+	[super setFrame:frame];
+	if (_rightText.hidden) {
+		_leftText.frame = CGRectMake(0, 0, self.bounds.size.width, self.frame.size.height);
+	} else {
+		int widthStatusField  = 100;
+		if (_leftText.stringValue.length == 0) {
+			widthStatusField = self.frame.size.width;
+		}
+		_leftText.frame = CGRectMake(0, 0, self.frame.size.width - widthStatusField, self.frame.size.height);
+		_rightText.frame = CGRectMake(self.frame.size.width - widthStatusField, 0, widthStatusField, self.frame.size.height);
+	}
+}
 
-- (void)setItemPropertiesToDefault:sender
-{
+- (void)setItemPropertiesToDefault:sender {
     self.displayProgress = YES;
 
-    _foregroundTextColor = [NSColor textColor];
- 
+	_foregroundTextColor = [NSColor textColor];
+	self.leftText.backgroundColor = [NSColor clearColor];
+	self.rightText.backgroundColor = [NSColor clearColor];
+
     _doubleValue = 0.0;
     _barColor = [NSColor colorWithCalibratedRed:1.0 green:.61 blue:.45 alpha:0.60];
     [self setAutoresizingMask:NSViewWidthSizable  ];
-    [self.leftText setAutoresizingMask:NSViewWidthSizable] ;
-    [self.rightText setAutoresizingMask:NSViewMinXMargin] ;
-
-
 }
 
-- (void)drawRect:(NSRect)dirtyRect
-{
+- (void)drawRect:(NSRect)dirtyRect {
     if (self.displayProgress) {
         NSBezierPath *thisPath = [NSBezierPath bezierPathWithRect:CGRectMake(0, 0, self.frame.size.width * _doubleValue, self.frame.size.height)];
         [_barColor set];
@@ -54,8 +62,7 @@
     }
 }
 
--(void)setDoubleValue:(double)doubleValue
-{
+-(void)setDoubleValue:(double)doubleValue {
 	_doubleValue = doubleValue;
 	[self setNeedsDisplay:YES];
 }
@@ -64,18 +71,8 @@
 -(void) setDisplayProgress: (BOOL) displayProgress {
     if (displayProgress != _displayProgress) {
         _displayProgress = displayProgress;
-        if (displayProgress) {
-			 int widthStatusField  = 70;
-			if (_leftText.stringValue.length == 0) {
-				widthStatusField = self.frame.size.width;
-			}
-            _leftText.frame = CGRectMake(0, 0, self.frame.size.width - widthStatusField, self.frame.size.height);
-            _rightText.frame = CGRectMake(self.frame.size.width - widthStatusField, 0, widthStatusField, self.frame.size.height);
-            _rightText.hidden = NO;
-        } else {
-            _leftText.frame = CGRectMake(0, 0, self.bounds.size.width, self.frame.size.height);
-            _rightText.hidden = YES;
-        }
+		_rightText.hidden = !displayProgress;
+		[self setFrame:self.frame];
         [self setNeedsDisplay:YES];
     }
 }
