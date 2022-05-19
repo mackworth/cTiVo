6250e35c7050c155976b3b865abdb85205ca1dab
mackworth <mackworth@aol.com>
2022-05-19 14:24:03 -0700

    Update certificate to 2024

--- cTiVo/MTTivoRPC.m
+++ cTiVo/MTTivoRPC.m
@@ -108,7 +108,7 @@ NSTimeInterval delay1, delay2, delay3,delay4 = 1.0;
         if (status != errSecSuccess) {
             DDLogReport(@"Could not create temporary keychain \"%@\": [%d] %@", keychainPath, (int)status, securityErrorMessageString(status));
         } else if (keychain) {
-			NSString * tivoPassword = @"XF7x4714qw";
+			NSString * tivoPassword = @"vlZaKoduom";
 
 			NSDictionary * optionsDictionary = @{(id)kSecImportExportPassphrase: tivoPassword,
 												 (id)kSecImportExportKeychain:   (__bridge id)keychain};
